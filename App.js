83584260464382bb04989cdbeec2f5b37c3d4296
iamgroot09 <68369550+iamgroot09@users.noreply.github.com>
2020-07-16 12:58:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavigationRouter from './component/NavigationRouter';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationRouter />
+    </div>
+  );
+}
+
+export default App;
